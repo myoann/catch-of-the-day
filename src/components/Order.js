51e6e1fcaa8a5e92c0f5ba412e0f2f2879da2431
Yoann <moise.yoann@gmail.com>
2016-11-17 14:24:54 +0100

    Added form to add new fishes

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Order extends React.Component {
   render() {
     return(
-      <h2>Not Found</h2>
+      <h2>Order</h2>
     )
   }
 }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,24 +2,31 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
   goToStore(event) {
     // stop the form from submitting
     event.preventDefault();
     console.log("changed URL");
     // first grab the text from the box
-
+    const storeId = this.storeInput.value;
     // second were going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`)
   }
 
   render() {
     return (
-      <form className="store-selector" onSubmit={ this.goToStore }>
+      <form className="store-selector" onSubmit={ (e) => this.goToStore(e) }>
         <h2>Please enter a store</h2>
         <input type="text" required placeholder="Store name" defaultValue={ getFunName() } ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store</button>
       </form>
     )
   }
+}
+
+// Makes the router available
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
